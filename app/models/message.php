85a437d002f7d8da0d97777dcb872408c56486c2
graeham blank <graeham@gravitee.nl>
2012-07-18 17:00:00 +0200

    message edit
    message edit

--- app/models/message.php
+++ app/models/message.php
@@ -2,14 +2,6 @@
 /**
  * Message
  *
- * PHP version 5
- *
- * @category Model
- * @package  Croogo
- * @version  1.0
- * @author   Fahad Ibnay Heylaal <contact@fahad19.com>
- * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
- * @link     http://www.croogo.org
  */
 class Message extends AppModel {
 /**
